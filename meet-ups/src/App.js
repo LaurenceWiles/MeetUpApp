625ca7714a9168d20ecd5b26ff8d7e99d2b0bc26
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-04-22 23:03:10 +0200

    created nav component

--- meet-ups/src/App.js
+++ meet-ups/src/App.js
@@ -2,13 +2,15 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavouritesPage from "./pages/Favourites";
 import { Routes, Route } from "react-router-dom";
+import MainNavigation from "./components/layout/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/new-meetups" element={<NewMeetupsPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
       </Routes>
     </div>
---
+++ meet-ups/src/components/layout/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <div>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetups">New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favourites">Favourites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
