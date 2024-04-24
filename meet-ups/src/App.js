d50969038001254f2fee49ff0146066f1a9ef4a1
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-04-24 13:33:23 +0200

    installed firebase

--- meet-ups/src/App.js
+++ meet-ups/src/App.js
@@ -2,18 +2,17 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavouritesPage from "./pages/Favourites";
 import { Routes, Route } from "react-router-dom";
-import MainNavigation from "./components/layout/layout/layout/MainNavigation";
+import Layout from "./components/layout/layout/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetups" element={<NewMeetupsPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- meet-ups/src/pages/NewMeetups.js
+++ meet-ups/src/pages/NewMeetups.js
@@ -1,5 +1,26 @@
+import NewMeetupForm from "../components/layout/layout/layout/meetups/NewMeetupForm";
+import { useNavigate } from "react-router";
+
 const NewMeetupsPage = () => {
-  return <div>New Meetups Page</div>;
+  const navigate = useNavigate();
+
+  const addMeetupHandler = (meetupData) => {
+    fetch("https://meetupsapp-44049-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/");
+    });
+  };
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetupsPage;
